212ba675f9bf335a1bbe3ff8097881477b0d7e53
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-06-20 02:57:58 +0100

    ok

--- app.py
+++ app.py
@@ -13,31 +13,37 @@ def healthChecktimer():
     # Managing Users Microservices Health Check
     Userhandler = UserService()
     Userhandler.create_service_url()
+    # Userhandlernum = Userhandler.number_of_services_in_each_microservices
     UserhandlerHealthCheck = Userhandler.process_services(Userhandler.services)
 
     # SMS Notifications Microservices Health Check
     SmsNotificationHandler = SmsService()
     SmsNotificationHandler.create_service_url()
+    # smsnum = SmsNotificationHandler.number_of_services_in_each_microservices
     SmsNotificationHandlerHealthCheck = SmsNotificationHandler.process_services(SmsNotificationHandler.services)
 
     # Managing Static and External Pages Microservices Health Check
     ManagePageHandler = ManagePageService()
     ManagePageHandler.create_service_url()
+    # managenum = ManagePageHandler.number_of_services_in_each_microservices
     ManagePageHandlerHealthCheck = ManagePageHandler.process_services(ManagePageHandler.services)
 
     # Email Notifications Microservices Health Check
     EmailHandler = EmailService()
     EmailHandler.create_service_url()
+    # emailnum = EmailHandler.number_of_services_in_each_microservices
     EmailHandlerHealthCheck = EmailHandler.process_services(EmailHandler.services)
 
     # Managing Company Settings Microservices Health Check
     CompanyHandler = CompanySettingsService()
     CompanyHandler.create_service_url()
+    # companynum = CompanyHandler.number_of_services_in_each_microservices
     CompanyHandlerHealthCheck = CompanyHandler.process_services(CompanyHandler.services)
 
     # Authentication Microservices Health Check
     AuthHandler = AuthService()
     AuthHandler.create_service_url()
+    # authnum = AuthHandler.number_of_services_in_each_microservices
     AuthHandlerHealthCheck = AuthHandler.process_services(AuthHandler.services)
 
     return {'Users':UserhandlerHealthCheck, 'Sms Notifications':SmsNotificationHandlerHealthCheck, 'Manage Pages':ManagePageHandlerHealthCheck, 'Email Services':EmailHandlerHealthCheck, 'Company Services':CompanyHandlerHealthCheck, 'Auth Services':AuthHandlerHealthCheck}
